7099c7a16aa1c875d6eb52dd6671754a4b40f009
Jeremy Keeler <6991379+jerkeeler@users.noreply.github.com>
2021-11-18 16:59:32 -0500

    Remove github link from Footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
 
-import Icon from './Icon';
-import ExternalLink from './ExternalLink';
-
 const Footer = () => (
   <footer className="text-center p-10 pt-0 flex items-center flex-col flex-grow justify-end">
-    <p>© {new Date().getFullYear()} Tim Keeler</p>
-    <p>
-      View code on{' '}
-      <ExternalLink to="https://github.com/jerkeeler/tim-keeler-gatsby">
-        <Icon icon="github" className="inline w-5" />
-      </ExternalLink>
-    </p>
+    <p>© 2019-{new Date().getFullYear()} Tim Keeler</p>
   </footer>
 );
 
